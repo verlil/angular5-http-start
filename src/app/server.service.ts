6b25e1799c3c89f0ed8c4e0272ec25041eff64b2
verlil <vernigoralilia@gmail.com>
2018-06-21 02:59:12 +0300

    sending headers

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -7,7 +7,11 @@ export class ServerService {
   }
 
   storeServers(servers: any[]) {
-    return this.http.post('https://angular5-http-start.firebaseio.com/data.json', servers);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    return this.http
+      .post('https://angular5-http-start.firebaseio.com/data.json',
+        servers,
+        {headers: headers);
     // return this.http.post('https://angular5-http-start.firebaseio.com/log.json', servers);
     // at this point we don't sending a request yet, because http service is using Observable
     // so sa there's no listener to responce, there's no reasons to send a request
